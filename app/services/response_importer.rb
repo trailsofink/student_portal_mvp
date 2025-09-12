01fedab04f3f769391546060e243eb7f4c3ec9ba
Joel Hunt <itsjoelhunt@gmail.com>
2025-09-12 12:04:09 -0500

    uploading and storing surveys questions, responses and answers based on qualtrics export csv.

--- app/services/response_importer.rb
+++ app/services/response_importer.rb
@@ -9,92 +9,82 @@ class ResponseImporter
     @file = file
   end
 
-  def process
-    csv = CSV.new(file.path, headers: false)
+  def get_qid(string)
+    match_data = string.match(/^QID\d+/)
+    match_data
+  end
 
+  def process
+    puts "[Importer] Starting file processing."
+    file_content = File.read(file.path)
+
+    csv = CSV.parse(file_content, headers: false)
+
+    if csv.length < 3
+      raise "Invalid CSV format: The file must contain at least 2 header rows and 1 data row."
+    end
+
+    # Deconstruct the array of rows
     headers = csv.shift
     questions_headings = csv.shift
-    qualtrics_metadata_row = csv.shift
+    data_rows = csv
+    qualtrics_question_id = headers
 
-    question_info = headers.zip(questions_headings).to_h
+    record_id_index = headers.index('_recordId')
+    unless record_id_index
+      raise "CSV file must have a '_recordId' column."
+    end
 
-    csv.each do |row|
-      row_data = Hash[headers.zip(row)]
+    csv_response_ids = data_rows.map { |row| row[record_id_index] }.compact.uniq
 
-      ActiveRecord::Base.transaction do
-        program_id = row_data["ProgramId__c"]
-        survey = Survey.find_or_create_by(program: program_id)
+    if csv_response_ids.any?
+      existing_response_count = Response.where(unique_id: csv_response_ids).count
 
-        response = survey.responses.create!(unique_id: row_data['ResponseId'], uploader_id: Current.user.id)
-
-        row_data.each do |header, value|
-          question = survey.questions.find_by(name: header)
-
-          unless question
-            question = survey.questions.create!(name: header, heading: question_info[header])
-          end
-
-          response.answer.create!(question: question, value: value)
-        end
+      if existing_response_count == csv_response_ids.length
+        raise "All responses in this file have already been imported."
       end
     end
+
+    question_info = headers.zip(questions_headings).to_h
+    puts "[Importer] Headers parsed."
+
+    data_rows.each_with_index do |row, index|
+      row_data = Hash[headers.zip(row)]
+
+      begin
+        ActiveRecord::Base.transaction do
+          program_id = row_data['ProgramID__c']
+          puts "[Importer] Program ID: #{program_id.inspect}"
+
+          survey = Survey.find_or_create_by!(program: program_id)
+          response_id = row_data['_recordId']
+
+          response = survey.responses.find_by(unique_id: response_id)
+          if response.present?
+            puts "[Importer] Response #{response_id} has already been imported! Skipping this row."
+            next
+          end
+          response = survey.responses.create!(unique_id: row_data['_recordId'], uploader_id: Current.user.id)
+
+          row_data.each do |header, value|
+            question = survey.questions.find_by(name: header)
+            unless question
+              question = survey.questions.create!(name: header, heading: question_info[header])
+            end
+
+            if question
+              response.answers.create!(question: question, value: value)
+            end
+          end
+        end
+      rescue => e
+        puts "!!!!! [Importer] ERROR on row #{index + 1}: #{e.message} !!!!!"
+        puts "[Importer] Failing row data: #{row_data.inspect}"
+        puts e.backtrace.join("\n")
+        raise e
+      end
+    end
+    puts "[Importer] Finished file processing."
     true
-  rescue StandardError => e
-    Rails.logger.error "Response import failed: #{e.message}"
-    false
   end
 end
-
-  
-  # attr_reader :file
-  #
-  # def initialize(file:)
-  #   @file = file
-  # end
-  #
-  # def process
-  # csv = CSV.new(file.path, headers: false)
-  #
-  #     headers = csv.shift
-  #     questions = csv.shift
-  #     qualtrics_import_ids = csv.shift
-  #     survey_id = Survey.find_by(program: row_hash['ProgramId__c'])
-  #
-  #   if !survey_id
-  #     new_survey = Survey.new
-  #     new_survey.program = row_hash['ProgramId__c']
-  #     new_survey.save
-  #     survey_id = new_survey.id
-  #   end
-  #
-  #   question_data = Hash[headers.zip(questions, qualtrics_import_ids)]
-  #
-  #   question_data.each do |header, question_heading, qualtrics_id|
-  #     question = Question.new
-  #     question.survey_id = survey_id
-  #     question.name = header
-  #     question.qualtrics_id = qualtrics_id
-  #     question.heading = question_heading
-  #     question.save
-  #   end
-  #
-  #   csv.each do |row|
-  #     ActiveRecord::Base.transaction do
-  #       row_hash = Hash[headers.zip(row)]
-  #       response = Response.new
-  #       response.unique_id = row_hash['ResponseId']
-  #       response.uploader_id = Current.user.id
-  #       response.survey_id = survey_id
-  #       response.save
-  #
-  #       row_hash.each do |header, value|
-  #         answer = Answer.new
-  #         answer.value = value
-  #         answer.response_id = response.id
-  #         answer.question_id = question.id
-  #         answer.save
-  #       end
-  #     end
-  #   end
-  # end
-  #end
