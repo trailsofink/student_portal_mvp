5f5200744daf59e1a9f99d6c99bf4b50c0957430
Joel Hunt <itsjoelhunt@gmail.com>
2025-09-16 10:57:53 -0500

    removing extension that causes render deploy error

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_12_203410) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  
 
   create_table "answers", force: :cascade do |t|
     t.integer "response_id"
