95da8a149bcc4adfa37c3dd6a6088cf1f724b48d
Joel Hunt <itsjoelhunt@gmail.com>
2025-09-16 12:09:09 -0500

    configering solid to match xata compatibility.

--- config/initializers/solid_database_connection.rb
+++ config/initializers/solid_database_connection.rb
@@ -1,4 +1,4 @@
-Rails.application.config.to_prepare do
+ActiveSupport.on_load(:active_record) do
   SolidCache::Record.connects_to database: { writing: :primary, reading: :primary }
   SolidQueue::Record.connects_to database: { writing: :primary, reading: :primary }
   SolidCable::Record.connects_to database: { writing: :primary, reading: :primary }
