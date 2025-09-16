2f9590a2211333841ed0d9e2e72bb566eb37e53a
Joel Hunt <itsjoelhunt@gmail.com>
2025-09-16 11:17:49 -0500

    copying rails-8 template compatibility.rb

--- config/initializers/00_xata_compatibility.rb
+++ config/initializers/00_xata_compatibility.rb
@@ -1,10 +1,14 @@
 # Xata database compatibility
 # Xata doesn't support CREATE EXTENSION statements but has plpgsql enabled by default
-Rails.application.config.to_prepare do
-  if ENV["DATABASE_URL"]&.include?("xata.sh")
+# This must run before schema loading, hence the 00_ prefix for early loading
+
+if ENV["DATABASE_URL"]&.include?("xata.sh")
+  # Use ActiveSupport.on_load to ensure the adapter is loaded before patching
+  ActiveSupport.on_load(:active_record) do
     module XataExtensionPrevention
       def enable_extension(name, **)
         Rails.logger&.info "Skipping extension '#{name}' - Xata has it enabled by default"
+        nil
       end
     end
 
