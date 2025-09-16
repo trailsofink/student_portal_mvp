72ac30a607d97b7337d9af111c7face6f95dc4e7
Joel Hunt <itsjoelhunt@gmail.com>
2025-09-16 11:03:19 -0500

    git add xata compatibility.

---
+++ config/initializers/00_xata_compatibility.rb
@@ -0,0 +1,11 @@
+# Xata database compatibility
+# Xata doesn't support CREATE EXTENSION statements but has plpgsql enabled by default
+if ENV["DATABASE_URL"]&.include?("xata.sh")
+  module XataExtensionPrevention
+    def enable_extension(name, **)
+      Rails.logger&.info "Skipping extension '#{name}' - Xata has it enabled by default"
+    end
+  end
+
+  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(XataExtensionPrevention)
+end
